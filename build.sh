331987f8c83145e839026b222cbad5966dd9cd66
Error404 <khvysal@gmail.com>
2026-07-23 17:15:28 +0700

    Add admin seeding script

--- build.sh
+++ build.sh
@@ -11,4 +11,8 @@ python manage.py migrate --noinput
 # Show status
 python manage.py showmigrations
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'vysal99999@gmail.com', 'Admin123@')" | python manage.py shell
+
+echo "Admin seeding completed."
+
 echo "Build completed."
