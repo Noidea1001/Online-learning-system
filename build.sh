c8ffb4c53950b031e352bb1584e696c6b56eb2f1
Error404 <khvysal@gmail.com>
2026-07-23 17:21:29 +0700

    Add secure auto-createsuperuser script

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 
-# Force migrate all apps
+# ដំណើរការបង្កើតតារាងក្នុង Database
 python manage.py migrate --noinput
+
+# បង្កើត Superuser ដោយស្វ័យប្រវត្តិតាមរយៈ Environment Variables ខាងលើ
+python manage.py createsuperuser --noinput || echo "Superuser already exists or creation skipped."
 
 # Show status
 python manage.py showmigrations
