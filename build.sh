80870b13deca119ce1d872afdbb58b8425a596d5
Error404 <khvysal@gmail.com>
2026-07-23 16:49:58 +0700

    Update build.sh for migrations

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 # exit on error
 set -o errexit
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Running migrations..."
 python manage.py migrate --noinput
 
-# Force apply all migrations again to be safe
-python manage.py migrate --noinput
+echo "Build completed successfully."
