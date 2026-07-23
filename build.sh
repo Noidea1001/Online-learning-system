90c0ca4fbd5dda0755413c3b645c2e99e0078254
Error404 <khvysal@gmail.com>
2026-07-23 17:00:49 +0700

    Update build.sh for migrations1

--- build.sh
+++ build.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Installing packages..."
 pip install -r requirements.txt
-
-echo "Collecting static..."
 python manage.py collectstatic --noinput
 
-echo "Making migrations..."
-python manage.py makemigrations --noinput
+echo "=== Making migrations for all apps ==="
+python manage.py makemigrations category lessons assignments submissions reviews enrollments employees courses students instructors users dashboard adminpanel quizzes notifications --noinput
 
-echo "Applying migrations..."
+echo "=== Applying all migrations ==="
 python manage.py migrate --noinput
 
-echo "Migration status:"
+echo "=== Migration Status ==="
 python manage.py showmigrations
-
-echo "Build done."
