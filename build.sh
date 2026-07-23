0ee7869d647905c136bcefa079e6e7a860c5d9af
Error404 <khvysal@gmail.com>
2026-07-23 16:48:18 +0700

    fix error2

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
+
+# Force apply all migrations again to be safe
+python manage.py migrate --noinput
